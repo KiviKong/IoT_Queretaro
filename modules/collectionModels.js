361eaada65c6bd2db1d3f488ade2046764919c50
KiviKong <kevin95ra@gmail.com>
2018-03-24 12:46:05 -0600

    Adds the dynamodb models

---
+++ modules/collectionModels.js
@@ -0,0 +1,30 @@
+const Usuario = {
+    deviceID: 45,
+    usuario: {
+        nombre: 'Kevin Castro',
+        edad: 22,
+        curp: 'KARC21312321321LKSJLK',
+        tel: '348 102 30 55'
+    },
+    registro: new Date(),
+    ruta_turistica: false,
+    x: 0,
+    y: 0,
+    x2: 0,
+    y2: 0,
+};
+
+const Visitas = {
+    deviceID: 45,
+    usuario: {
+        nombre: 'Kevin Castro',
+        edad: 22,
+        curp: 'KARC21312321321LKSJLK',
+        tel: '348 102 30 55'
+    },
+    fecha_registro: new Date(),
+    fecha_salida: new Date(),
+    edificio_destino: 'Rectoria',
+    ruta_turistica: true,
+    modulos: [], //array de modulos consultados 
+}
